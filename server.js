e71fc130feb7b8a2620340b9abe1890cb8122875
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-06-10 17:44:07 +0530

    Update cors in server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const cookie = require('cookie-parser');
+const { clientURL } = require('./utils/secrets');
 
 const authRouter = require("./Routes/auth.router")
 
-app.use(cors())
+app.use(cors(clientURL))
 app.use(express.json());
 app.use(express.urlencoded(true))
 app.use(cookie())
