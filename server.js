80c5145ebb1bce93314de5ca912baac1af83ee2b
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-06-13 19:40:31 +0530

    Fix cors issues

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const { clientURL } = require('./utils/secrets');
 const authRouter = require("./Routes/auth.router")
 const expensesRouter = require("./Routes/expenses.router")
 
-app.use(cors(clientURL))
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', clientURL);
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Credentials', true);
+    next();
+})
 app.use(express.json());
-app.use(express.urlencoded(true))
 app.use(cookie())
-
 app.get("/", (req, res) => res.send("Live"))
 app.use(authRouter)
 app.use(expensesRouter)
