331f4c8fa7686e9ee48e3a8a9cb756770457fe48
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-06-24 10:43:08 +0530

    fix events router

--- Routes/events.router.js
+++ Routes/events.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { auth } = require("../Controllers/auth.controller");
 const { addEvent, getEvents, verifyAccessToEvents, editEvent, deleteEvent, verifyAccess } = require('../Controllers/events.controller');
 router.post("/api/events/addEvents", auth, verifyAccessToEvents, addEvent);
-router.post("/api/events/getAllEvents", auth, verifyAccessToEvents, getEvents);
+router.post("/api/events/getAllEvents", auth, getEvents);
 router.post("/api/events/editEventById", auth, verifyAccessToEvents, editEvent);
 router.post("/api/events/deleteEventById", auth, verifyAccessToEvents, deleteEvent);
 router.post("/api/events/verifyAccess", auth, verifyAccessToEvents, verifyAccess);
