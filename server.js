2973416eb6409d24ac192914c4705100d1634244
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-08-01 23:04:02 +0530

    Fix cors to allow old website

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const cookie = require('cookie-parser');
-const { clientURL } = require('./utils/secrets');
+const { clientURL_1, clientURL_2 } = require('./utils/secrets');
 
 const authRouter = require("./Routes/auth.router")
 const inventoryRouter = require("./Routes/inventory.router")
@@ -11,13 +11,32 @@ const expensesRouter = require("./Routes/expenses.router")
 const eventsRouter = require("./Routes/events.router")
 const usersRouter = require("./Routes/users.router")
 
-app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', clientURL);
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Allow-Credentials', true);
-    next();
-})
+var whitelist = [clientURL_1, clientURL_2];
+
+var corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	credentials:true,
+	allowedHeaders:['Content-Type'],
+}
+
+app.options("*",cors(corsOptions));
+app.use(cors(corsOptions));
+
+//Do not delete the following commented lines, keep this to know what headers are to be set
+
+//app.use((req, res, next) => {
+//    res.set('Access-Control-Allow-Origin', clientURL);
+//    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//    res.set('Access-Control-Allow-Headers', 'Content-Type');
+//    res.set('Access-Control-Allow-Credentials', true);
+//    next();
+//})
 app.use(express.json());
 app.use(cookie())
 app.get("/", (req, res) => res.send("Live"))
