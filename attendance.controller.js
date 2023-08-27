78500b94bde2289133e7ca7cd673e114411c62d4
Srivatsa <srivatsarupadhya@gmail.com>
2023-08-28 03:11:23 +0530

    revert back to 8b2463c

---
+++ attendance.controller.js
@@ -0,0 +1,107 @@
+const prisma = require("../utils/db");
+const asyncWrapper = require("../utils/asyncWrapper");
+const fs = require("fs");
+const os = require("os");
+const updateAttendance = async (req, res) => {
+	await asyncWrapper(req, res, async (req, res) => {
+		const user = res.locals.user;
+		if (user.hasAccessTo === "ADMIN" || user.hasAccessTo === "SUPERUSER") {
+			try {
+				const { AttendanceList } = req.body;
+
+				const update = Promise.all(
+					AttendanceList.map(async (element) => {
+						try {
+							await prisma.users.updateMany({
+								where: {
+									userID: element,
+								},
+								data: {
+									attendance: { increment: 1 },
+								},
+							});
+						} catch (mapError) {
+							console.log(mapError);
+							throw mapError;
+						}
+					})
+				);
+				await update;
+				return res.status(200).json({ message: "success" });
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		} else {
+			return res.status(403).send("Not Authorized!");
+		}
+	});
+};
+
+const getAttendanceList = async (req, res) => {
+	await asyncWrapper(req, res, async (req, res) => {
+		const user = res.locals.user;
+		if (user.hasAccessTo === "ADMIN" || user.hasAccessTo === "SUPERUSER") {
+			try {
+				const result = await prisma.users.findMany({
+					select: { name: true, IDCardNum: true, attendance: true },
+				});
+				return res
+					.status(200)
+					.json({ message: "success", data: result });
+			} catch (error) {
+				console.log(error);
+				throw error;
+			}
+		} else {
+			return res.status(403).send("Not Authorized!");
+		}
+	});
+};
+
+const dowonloadAttendanceList = async (req, res) => {
+	await asyncWrapper(req, res, async (req, res) => {
+		const user = res.locals.user;
+		if (user.hasAccessTo === "ADMIN" || user.hasAccessTo === "SUPERUSER") {
+			try {
+				await fs.promises.appendFile(
+					"list.csv",
+					`Name,RCNID,Number of classes attended`
+				);
+
+				const users = await prisma.users.findMany({
+					select: { name: true, IDCardNum: true, attendance: true },
+				});
+				await Promise.all(
+					users.map(async (element) => {
+						try {
+							await fs.promises.appendFile(
+								"list.csv",
+								`${os.EOL}${element.name},${element.IDCardNum},${element.attendance}`
+							);
+						} catch (error) {
+							throw error;
+						}
+					})
+				);
+				res.download("list.csv", (err) => {
+					if (err) {
+						console.log(err);
+					}
+				});
+				await fs.promises.rm("list.csv");
+			} catch (error) {
+				console.log(error);
+				return res.status(200).json({ message: "An error occurred!" });
+			}
+		} else {
+			return res.status(403).send("Not Authorized!");
+		}
+	});
+};
+
+module.exports = {
+	updateAttendance,
+	getAttendanceList,
+	dowonloadAttendanceList,
+};
---
+++ attendance.router.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { auth, userStatus } = require("../Controllers/auth.controller");
+const {
+	updateAttendance,
+	getAttendanceList,
+	dowonloadAttendanceList,
+} = require("../Controllers/attendance.controller");
+
+router.post(
+	"/api/attendance/submitAttendance",
+	auth,
+	userStatus,
+	updateAttendance
+);
+router.get(
+	"/api/attendance/getAttendanceList",
+	auth,
+	userStatus,
+	getAttendanceList
+);
+router.get(
+	"/api/attendance/downloadAttendance",
+	auth,
+	userStatus,
+	dowonloadAttendanceList
+);
+
+module.exports = router;
