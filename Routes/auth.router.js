16d50d3de664a287f3019a8a2c990b760b248c74
Srivatsa <srivatsarupadhya@gmail.com>
2023-08-28 19:09:35 +0530

    add delete user using userid

--- Routes/auth.router.js
+++ Routes/auth.router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { register, me, logout, auth, deleteAccount, userStatus} = require('../Controllers/auth.controller');
+const { register, me, logout, auth, deleteAccount, userStatus, deleteAccountByUserId} = require('../Controllers/auth.controller');
 
 
 router.post("/api/auth/register", register);
 router.post("/api/auth/me", auth,userStatus, me);
 router.delete("/api/auth/logout", logout)
 router.post("/api/auth/deleteAccount",auth,userStatus, deleteAccount)
-router.post("/api/auth/deleteAccount",auth,userStatus, deleteAccount)
+router.post("/api/auth/deleteAccountByUserId",auth,userStatus, deleteAccountByUserId)
 
 module.exports = router; 
