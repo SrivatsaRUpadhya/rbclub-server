b0729c37b0b208414a99752d44c1ed76b3ec2d31
Srivatsa <srivatsarupadhya@gmail.com>
2023-08-31 09:32:34 +0530

    log memory usage info

--- server.ts
+++ server.ts
@@ -65,6 +65,27 @@ try {
 	app.listen(process.env.PORT, () => {
 		console.log(`Server live on port: ${process.env.PORT}`);
 	});
+	const formatMemoryUsage = (data: any) =>
+		`${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
+
+	const memoryData = process.memoryUsage();
+
+	const memoryUsage = {
+		rss: `${formatMemoryUsage(
+			memoryData.rss
+		)} -> Resident Set Size - total memory allocated for the process execution`,
+		heapTotal: `${formatMemoryUsage(
+			memoryData.heapTotal
+		)} -> total size of the allocated heap`,
+		heapUsed: `${formatMemoryUsage(
+			memoryData.heapUsed
+		)} -> actual memory used during the execution`,
+		external: `${formatMemoryUsage(
+			memoryData.external
+		)} -> V8 external memory`,
+	};
+
+	console.log(memoryUsage);
 } catch (error) {
 	console.log(error);
 }
